70d26b94bb0260560e3792ce83e6914f090eb7bb
Mike Pattyn <mike@givtapp.net>
2019-10-17 13:31:03 +0200

    add command to validate raid command x

--- src/helpers/ValidationHelper.ts
+++ src/helpers/ValidationHelper.ts
@@ -1,4 +1,8 @@
 export class ValidationHelper {
+    static isValidRaidCommand(input: string) {
+        var regExPattern = new RegExp("(!|\\?{1})(raid)( \\w+){1,}");
+        return regExPattern.test(input);
+    }
     static isValidRaidRequestCommand(input: string) {
         var regExPattern = new RegExp("(!|\\?{1})(raid)( )(start|help)( )([Tt])([1-5]{1})(( \\w+){1,4})( )(0{2}|([0-9]|1[0-9]|2[0-3]))([Uu]|\\:)(0{2}|([0-5][0-9]))");
         return regExPattern.test(input);
