fde8b0b1de61cee5c9452e5f7f600f0ed387d7a6
Mike Pattyn <mike@givtapp.net>
2020-01-17 08:20:04 +0100

    add dtEnd getter and build date obj

--- src/services/google-cloud-vision.client.ts
+++ src/services/google-cloud-vision.client.ts
@@ -34,22 +34,36 @@ export interface IGymInfo {
     pokemon: string | null
     cp: string | null
     time_left: string | null
+    dtEnd: Date | null
 }
 
 export class GymInfo implements IGymInfo {
     titel: string | null;    
     pokemon: string | null;
     cp: string | null;
-    time_left: string | null;
+    private _time_left: string | null;
+    
+    get dtEnd() {
+        var timeObjects = this._time_left!!.split(':')
+        var date = new Date();
+        var totalSecondsLeft = (Number(timeObjects[0]) * 60 * 60) + (Number(timeObjects[1]) * 60) + Number(timeObjects[2])
+        date = new Date(date.setTime(date.getTime() + (totalSecondsLeft * 1000)))
+        return date;
+    }
+    get time_left() {
+        return this._time_left
+    }
     constructor(info: string[]){
         this.titel = info[3]
         this.cp = info[4]
         this.pokemon = info[5]
-        this.time_left = info[6]
+        this._time_left = info[6]
     }
 
+
+
     toString() { 
-        return `Found a ${this.pokemon} with ${this.cp} at${this.titel} - Ends at: ${this.time_left}`
+        return `Found a ${this.pokemon} with ${this.cp} at${this.titel} - Ends at: ${this.dtEnd}`
     }
 }
 export interface IPokestopInfo {
