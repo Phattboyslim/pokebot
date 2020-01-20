c1d26655f0b76b2374b0887afa71101e3ce05011
Mike Pattyn <mike@givtapp.net>
2020-01-20 20:20:19 +0100

    create custom class with a capitalizeFirstLetter func

--- src/clients/discord.client.ts
+++ src/clients/discord.client.ts
@@ -78,3 +78,12 @@ export class DiscordClient {
     }
 }
 
+export class CustomString { 
+    private _input: string;
+    constructor(input: string) {
+        this._input = input;
+    }
+    capitalizeFirstLetter() {
+        return this._input[0].toUpperCase + this._input.substring(1) 
+    }
+}
